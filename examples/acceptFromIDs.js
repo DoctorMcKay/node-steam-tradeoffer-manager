034cd10ad13b02281fe329a5f5bd0e2215c3fff9
Arze1 <Arze1@users.noreply.github.com>
2016-12-10 12:24:21 -0500

    Cleaned out code + removed exists func

--- examples/acceptFromIDs.js
+++ examples/acceptFromIDs.js
@@ -1,6 +1,16 @@
-var SteamUser = require('steam-user');
-var client = new SteamUser();
-var TradeOfferManager = require('steam-tradeoffer-manager');
+const SteamUser = require('steam-user'),
+SteamTotp = require('steam-totp'),
+TradeOfferManager = require('steam-tradeoffer-manager'),
+SteamCommunity = require('steamcommunity');
+
+var client = new SteamUser(),
+community = new SteamCommunity(),
+manager = new TradeOfferManager({
+	"steam": client,
+	"domain": "nothinghere",
+	"language": "en"
+});
+
 var config = {
 	"username": "",
 	"password": "",
@@ -12,9 +22,8 @@ var config = {
 		"SteamID"
   	]
 };
-var SteamTotp = require('steam-totp');
-var SteamCommunity = require('steamcommunity');
-var community = new SteamCommunity();
+
+
 function logOn() {
 	client.logOn({
 		"accountName": config.username,
@@ -38,11 +47,7 @@ client.on('loggedOn', function(details) {
 	client.setPersona(SteamUser.EPersonaState.Online);
 });
 
-var manager = new TradeOfferManager({
-	"steam": client,
-	"domain": "nothinghere",
-	"language": "en"
-});
+
 
 client.on('webSession', function(sessionID, cookies) {
 	manager.setCookies(cookies, function(err) {
@@ -58,24 +63,14 @@ client.on('webSession', function(sessionID, cookies) {
 	community.setCookies(cookies);
 	community.startConfirmationChecker(30000, config.identitySecret);
 });
-
-function exists(id) {
-  for (var i = 0; i < config.acceptSteamIDS.length; i++) {
-    if (config.acceptSteamIDS[i] == id) {
-      return true;
-    }
-    return false;
-  }
-}
-
 manager.on('newOffer', function(offer) {
 	console.log("New offer #" + offer.id + " from " + offer.partner.getSteamID64());
-	if (exists(offer.partner.getSteamID64())) {
+	if (config.acceptSteamIDS.indexOf(offer.partner.getSteamID64()) >= 0) {
 		offer.accept(function(err) {
 			if (err) {
 				console.log("Unable to accept offer: " + err.message);
 			} else if (exists(offer.partner.getSteamID64()) == true) {
-			  console.log("Offer Accepted from "+offer.partner.getSteamID64());
+				console.log("Offer Accepted from "+offer.partner.getSteamID64());
 			}
 		});
 	}
