cd4fa61f08e35d66e2e15345c969606500de716b
Maxim Stepanov <admin@bearwm.com>
2018-03-13 19:59:14 +0200

    ES6 examples

--- examples/offloader.js
+++ examples/offloader.js
@@ -8,77 +8,73 @@ const SteamUser = require('steam-user');
 const SteamCommunity = require('steamcommunity');
 const SteamTotp = require('steam-totp');
 const TradeOfferManager = require('../lib/index.js'); // use require('steam-tradeoffer-manager') in production
-const FS = require('fs');
+const fs = require('fs');
 
-let client = new SteamUser();
-let manager = new TradeOfferManager({
-	"steam": client, // Polling every 30 seconds is fine since we get notifications from Steam
-	"domain": "example.com", // Our domain is example.com
-	"language": "en" // We want English item descriptions
+const client = new SteamUser();
+const manager = new TradeOfferManager({
+	steam: client, // Polling every 30 seconds is fine since we get notifications from Steam
+	domain: 'example.com', // Our domain is example.com
+	language: 'en' // We want English item descriptions
 });
-let community = new SteamCommunity();
+const community = new SteamCommunity();
 
 // Steam logon options
-let logOnOptions = {
-	"accountName": "username",
-	"password": "password",
-	"twoFactorCode": SteamTotp.getAuthCode("sharedSecret")
+const logOnOptions = {
+	accountName: 'username',
+	password: 'password',
+	twoFactorCode: SteamTotp.getAuthCode('sharedSecret')
 };
 
-if (FS.existsSync('polldata.json')) {
-	manager.pollData = JSON.parse(FS.readFileSync('polldata.json').toString('utf8'));
+if (fs.existsSync('polldata.json')) {
+	manager.pollData = JSON.parse(fs.readFileSync('polldata.json').toString('utf8'));
 }
 
 client.logOn(logOnOptions);
 
-client.on('loggedOn', function() {
-	console.log("Logged into Steam");
+client.on('loggedOn', () => {
+	console.log('Logged into Steam');
 });
 
-client.on('webSession', function(sessionID, cookies) {
-	manager.setCookies(cookies, function(err) {
+client.on('webSession', (sessionID, cookies) => {
+	manager.setCookies(cookies, (err) => {
 		if (err) {
 			console.log(err);
 			process.exit(1); // Fatal error since we couldn't get our API key
 			return;
 		}
 
-		console.log("Got API key: " + manager.apiKey);
+		console.log(`Got API key: ${manager.apiKey}`);
 
 		// Get our inventory
-		manager.getInventoryContents(730, 2, true, function(err, inventory) {
+		manager.getInventoryContents(730, 2, true, (err, inventory) => {
 			if (err) {
 				console.log(err);
 				return;
 			}
 
-			if (inventory.length == 0) {
+			if (inventory.length === 0) {
 				// Inventory empty
-				console.log("CS:GO inventory is empty");
+				console.log('CS:GO inventory is empty');
 				return;
 			}
 
-			console.log("Found " + inventory.length + " CS:GO items");
+			console.log(`Found ${inventory.length} CS:GO items`);
 
 			// Create and send the offer
-			let offer = manager.createOffer("https://steamcommunity.com/tradeoffer/new/?partner=12345678&token=xxxxxxxx");
+			const offer = manager.createOffer('https://steamcommunity.com/tradeoffer/new/?partner=12345678&token=xxxxxxxx');
 			offer.addMyItems(inventory);
-			offer.setMessage("Here, have some items!");
-			offer.send(function(err, status) {
+			offer.setMessage('Here, have some items!');
+			offer.send((err, status) => {
 				if (err) {
 					console.log(err);
 					return;
 				}
 
-				if (status == 'pending') {
+				if (status === 'pending') {
 					// We need to confirm it
 					console.log(`Offer #${offer.id} sent, but requires confirmation`);
-					community.acceptConfirmationForObject("identitySecret", offer.id, function(err) {
-						if (err) {
-							console.log(err);
-						} else {
-							console.log("Offer confirmed");
-						}
+					community.acceptConfirmationForObject('identitySecret', offer.id, (err) => {
+						err ? err : console.log('Offer confirmed');
 					});
 				} else {
 					console.log(`Offer #${offer.id} sent successfully`);
@@ -90,12 +86,12 @@ client.on('webSession', function(sessionID, cookies) {
 	community.setCookies(cookies);
 });
 
-manager.on('sentOfferChanged', function(offer, oldState) {
+manager.on('sentOfferChanged', (offer, oldState) => {
 	console.log(`Offer #${offer.id} changed: ${TradeOfferManager.ETradeOfferState[oldState]} -> ${TradeOfferManager.ETradeOfferState[offer.state]}`);
 });
 
-manager.on('pollData', function(pollData) {
-	FS.writeFileSync('polldata.json', JSON.stringify(pollData));
+manager.on('pollData', (pollData) => {
+	fs.writeFileSync('polldata.json', JSON.stringify(pollData));
 });
 
 /*
--- examples/storehouse-steamcommunity.js
+++ examples/storehouse-steamcommunity.js
@@ -8,64 +8,64 @@
 const SteamCommunity = require('steamcommunity');
 const SteamTotp = require('steam-totp');
 const TradeOfferManager = require('../lib/index.js'); // use require('steam-tradeoffer-manager') in production
-const FS = require('fs');
+const fs = require('fs');
 
-let steam = new SteamCommunity();
-let manager = new TradeOfferManager({
-	"domain": "example.com", // Our domain is example.com
-	"language": "en", // We want English item descriptions
-	"pollInterval": 5000 // We want to poll every 5 seconds since we don't have Steam notifying us of offers
+const steam = new SteamCommunity();
+const manager = new TradeOfferManager({
+	domain: 'example.com', // Our domain is example.com
+	language: 'en', // We want English item descriptions
+	pollInterval: 5000 // We want to poll every 5 seconds since we don't have Steam notifying us of offers
 });
 
 // Steam logon options
-let logOnOptions = {
-	"accountName": "username",
-	"password": "password",
-	"twoFactorCode": SteamTotp.getAuthCode("sharedSecret")
+const logOnOptions = {
+	accountName: 'username',
+	password: 'password',
+	twoFactorCode: SteamTotp.getAuthCode('sharedSecret')
 };
 
-if (FS.existsSync('steamguard.txt')) {
-	logOnOptions.steamguard = FS.readFileSync('steamguard.txt').toString('utf8');
+if (fs.existsSync('steamguard.txt')) {
+	logOnOptions.steamguard = fs.readFileSync('steamguard.txt').toString('utf8');
 }
 
-if (FS.existsSync('polldata.json')) {
-	manager.pollData = JSON.parse(FS.readFileSync('polldata.json').toString('utf8'));
+if (fs.existsSync('polldata.json')) {
+	manager.pollData = JSON.parse(fs.readFileSync('polldata.json').toString('utf8'));
 }
 
-steam.login(logOnOptions, function(err, sessionID, cookies, steamguard) {
+steam.login(logOnOptions, (err, sessionID, cookies, steamguard) => {
 	if (err) {
-		console.log("Steam login fail: " + err.message);
+		console.log(`Steam login fail:  ${err.message}`);
 		process.exit(1);
 	}
 
-	FS.writeFileSync('steamguard.txt', steamguard);
+	fs.writeFileSync('steamguard.txt', steamguard);
 
-	console.log("Logged into Steam");
+	console.log('Logged into Steam');
 
-	manager.setCookies(cookies, function(err) {
+	manager.setCookies(cookies, err => {
 		if (err) {
 			console.log(err);
 			process.exit(1); // Fatal error since we couldn't get our API key
 			return;
 		}
 
-		console.log("Got API key: " + manager.apiKey);
+		console.log(`Got API key: ${manager.apiKey}`);
 	});
 });
 
-manager.on('newOffer', function(offer) {
-	console.log("New offer #" + offer.id + " from " + offer.partner.getSteam3RenderedID());
-	offer.accept(function(err, status) {
+manager.on('newOffer', offer => {
+	console.log(`New offer #${offer.id} from ${offer.partner.getSteam3RenderedID()}`);
+	offer.accept((err, status) => {
 		if (err) {
-			console.log("Unable to accept offer: " + err.message);
+			console.log(`Unable to accept offer: ${err.message}`);
 		} else {
-			console.log("Offer accepted: " + status);
-			if (status == "pending") {
-				steam.acceptConfirmationForObject("identitySecret", offer.id, function(err) {
+			console.log(`Offer accepted: ${status}`);
+			if (status === 'pending') {
+				steam.acceptConfirmationForObject('identitySecret', offer.id, err => {
 					if (err) {
-						console.log("Can't confirm trade offer: " + err.message);
+						console.log(`Can't confirm trade offer: ${err.message}`);
 					} else {
-						console.log("Trade offer " + offer.id + " confirmed");
+						console.log(`Trade offer ${offer.id} confirmed`);
 					}
 				});
 			}
@@ -73,27 +73,27 @@ manager.on('newOffer', function(offer) {
 	});
 });
 
-manager.on('receivedOfferChanged', function(offer, oldState) {
+manager.on('receivedOfferChanged', (offer, oldState) => {
 	console.log(`Offer #${offer.id} changed: ${TradeOfferManager.ETradeOfferState[oldState]} -> ${TradeOfferManager.ETradeOfferState[offer.state]}`);
 
-	if (offer.state == TradeOfferManager.ETradeOfferState.Accepted) {
+	if (offer.state === TradeOfferManager.ETradeOfferState.Accepted) {
 		offer.getExchangeDetails((err, status, tradeInitTime, receivedItems, sentItems) => {
 			if (err) {
-				console.log(`Error ${err}`);
+				console.log(`Error: ${err}`);
 				return;
 			}
 
 			// Create arrays of just the new assetids using Array.prototype.map and arrow functions
-			let newReceivedItems = receivedItems.map(item => item.new_assetid);
-			let newSentItems = sentItems.map(item => item.new_assetid);
+			const newReceivedItems = receivedItems.map(item => item.new_assetid);
+			const newSentItems = sentItems.map(item => item.new_assetid);
 
 			console.log(`Received items ${newReceivedItems.join(',')} Sent Items ${newSentItems.join(',')} - status ${TradeOfferManager.ETradeStatus[status]}`)
 		})
 	}
 });
 
-manager.on('pollData', function(pollData) {
-	FS.writeFileSync('polldata.json', JSON.stringify(pollData));
+manager.on('pollData', (pollData) => {
+	fs.writeFileSync('polldata.json', JSON.stringify(pollData));
 });
 
 /*
